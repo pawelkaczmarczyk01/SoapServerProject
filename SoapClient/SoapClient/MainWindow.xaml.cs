6fb9d33c555deafac34c1ffce0906a0affd6bbc3
Michał Kuc <mkkuc1@gmail.com>
2020-04-12 20:06:11 +0200

    Przygotowanie aplikacji wpf wraz z przygotowanym odwołaniem do projektu soap z projektu java

---
+++ SoapClient/SoapClient/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SoapClient.HotelSoap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoapClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetValue();
+        }
+
+        private string GetValue()
+        {
+            var client = new HotelsPortClient();
+            var request = new getNameRequest();
+            var response = client.getName(request);
+            return response.name;
+        }
+    }
+}
