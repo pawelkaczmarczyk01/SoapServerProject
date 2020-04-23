8f4e64613300c7001893f4d2439d198bbc715652
pkaczmarczyk <pawel.kaczmarczyk@artifact.pl>
2020-04-23 15:35:23 +0200

    change hotelImage from bytea to string

--- src/main/java/com/example/SoapServerProject/config/WebServiceConfig.java
+++ src/main/java/com/example/SoapServerProject/config/WebServiceConfig.java
@@ -6,19 +6,53 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.endpoint.adapter.DefaultMethodEndpointAdapter;
+import org.springframework.ws.server.endpoint.adapter.method.MarshallingPayloadMethodProcessor;
+import org.springframework.ws.server.endpoint.adapter.method.MethodArgumentResolver;
+import org.springframework.ws.server.endpoint.adapter.method.MethodReturnValueHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EnableWs
 @Configuration
 @EnableAsync
 public class WebServiceConfig extends WsConfigurerAdapter {
+
+//    @Bean
+//    public Jaxb2Marshaller marshaller() {
+//        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+//        marshaller.setPackagesToScan("com.example.SoapServerProject.web");
+//        marshaller.setMtomEnabled(true);
+//        return marshaller;
+//    }
+//
+//    @Bean
+//    @Override
+//    public DefaultMethodEndpointAdapter defaultMethodEndpointAdapter() {
+//        List<MethodArgumentResolver> argumentResolvers = new ArrayList<>();
+//        argumentResolvers.add(methodProcessor());
+//        List<MethodReturnValueHandler> returnValueHandlers = new ArrayList<>();
+//        returnValueHandlers.add(methodProcessor());
+//        DefaultMethodEndpointAdapter adapter = new DefaultMethodEndpointAdapter();
+//        adapter.setMethodArgumentResolvers(argumentResolvers);
+//        adapter.setMethodReturnValueHandlers(returnValueHandlers);
+//        return adapter;
+//    }
+//
+//    @Bean
+//    public MarshallingPayloadMethodProcessor methodProcessor() {
+//        return new MarshallingPayloadMethodProcessor(marshaller());
+//    }
 
     @Bean("threadPoolTaskExecutor")
     public TaskExecutor getAsyncExecutor() {
